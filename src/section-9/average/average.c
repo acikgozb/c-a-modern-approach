21b3338f72ae56308de0357d619e34a4e44313ac
acikgozb <acikgozb@proton.me>
2025-08-13 16:20:35 +0300

    feat(average): Add `average.c` example
    The author officially starts functions in C with a simple example that
    computes the average of given numbers.
    
    Signed-off-by: acikgozb <acikgozb@proton.me>

---
+++ src/section-9/average/average.c
@@ -0,0 +1,16 @@
+// Computes pairwise averages of three numbers.
+
+#include <stdio.h>
+
+double average(double x, double y) { return (x + y) / 2; }
+
+int main(void) {
+  printf("enter three numbers: ");
+
+  double n1, n2, n3;
+  scanf("%lf%lf%lf", &n1, &n2, &n3);
+
+  printf("average of %g and %g: %g\n", n1, n2, average(n1, n2));
+  printf("average of %g and %g: %g\n", n1, n3, average(n1, n3));
+  printf("average of %g and %g: %g\n", n2, n3, average(n2, n3));
+}
