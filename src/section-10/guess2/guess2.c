1e5efe7275e880220895d4b8e8740ba5aac0b1a5
acikgozb <acikgozb@proton.me>
2025-08-14 23:21:12 +0300

    feat(guess): Add `guess.c` and `guess2.c` examples
    The author starts the chapter by introducing external (global) variables
    and the potential dangers of using them.
    
    `guess.c` and `guess2.c` are provided to demonstrate that the same
    functionality can be achieved without using external variables.
    
    Signed-off-by: acikgozb <acikgozb@proton.me>

---
+++ src/section-10/guess2/guess2.c
@@ -0,0 +1,57 @@
+// Asks user to guess a hidden number.
+// This provides the same functionality as `guess.c`, but without using
+// any external (global) variables.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_NUMBER 100
+
+void initialize_number_generator(void);
+int choose_new_secret_number(void);
+void read_guesses(int secret_number);
+
+int main(void) {
+  char command;
+  printf("guess the secret number between 1 and %d.\n\n", MAX_NUMBER);
+
+  initialize_number_generator();
+
+  do {
+    int secret_number = choose_new_secret_number();
+    printf("a new number has been chosen.\n");
+
+    read_guesses(secret_number);
+
+    printf("play again? (Y/N) ");
+    scanf(" %c", &command);
+
+    printf("\n");
+  } while (command == 'y' || command == 'Y');
+
+  return 0;
+}
+
+void initialize_number_generator(void) { srand((unsigned)time(NULL)); }
+
+int choose_new_secret_number(void) { return rand() % MAX_NUMBER + 1; }
+
+void read_guesses(int secret_number) {
+  int guess, num_guesses = 0;
+
+  for (;;) {
+    num_guesses++;
+    printf("enter guess: ");
+    scanf("%d", &guess);
+
+    if (guess == secret_number) {
+      printf("you won in %d guesses!\n\n", num_guesses);
+      return;
+    } else if (guess < secret_number) {
+      printf("too low; try again.\n");
+    } else {
+      printf("too high; try again.\n");
+    }
+  }
+}
